c99c460fe3c577cd69c1d34257ae2809ea7287c6
Evgeniy Khramkov <v-evkhra@microsoft.com>
2017-05-08 18:22:38 +0300

    integration test files

--- spec/mobile_center_action_spec.rb
+++
@@ -1,9 +0,0 @@
-describe Fastlane::Actions::MobileCenterAction do
-  describe '#run' do
-    it 'prints a message' do
-      expect(Fastlane::UI).to receive(:message).with("The mobile_center plugin is working!")
-
-      Fastlane::Actions::MobileCenterAction.run(nil)
-    end
-  end
-end
