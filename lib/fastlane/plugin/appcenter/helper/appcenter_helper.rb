302cd3d2fde40ebc2b90fe0c46baacb029ed380b
Evgenii Khramkov <evkhramk@microsoft.com>
2019-06-10 11:10:27 -0700

    remove comment

--- lib/fastlane/plugin/appcenter/helper/appcenter_helper.rb
+++ lib/fastlane/plugin/appcenter/helper/appcenter_helper.rb
@@ -3,7 +3,6 @@ module Fastlane
     class AppcenterHelper
 
       # create request
-      # TODO: handle dsym vs mapping
       def self.connection(upload_url = false, dsym = false)
         require 'faraday'
         require 'faraday_middleware'
