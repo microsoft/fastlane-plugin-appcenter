ebda37f4d62e250f93e68ac2a14cd305f5330e45
Jean-Philippe ANDRE <jeandr@microsoft.com>
2019-11-03 10:38:23 +0900

    Special case .app.zip extension utility

--- lib/fastlane/plugin/appcenter/helper/appcenter_helper.rb
+++ lib/fastlane/plugin/appcenter/helper/appcenter_helper.rb
@@ -6,13 +6,9 @@ module Fastlane
       # accounting for file types that can and should be zip-compressed
       # before they are uploaded
       def self.file_extname_full(path)
-        is_zip = File.extname(path) == ".zip"
+        return ".app.zip" if path.end_with? ".app.zip"
 
-        # if file is not .zip'ed, these do not change basename and extname
-        unzip_basename = File.basename(path, ".zip")
-        unzip_extname = File.extname(unzip_basename)
-
-        is_zip ? unzip_extname + ".zip" : unzip_extname
+        File.extname path
       end
 
       # create request
