bb3f8975ee243c5e9df4d6997a0611dd41d6a646
Jack Spargo <jspargo@shopkeep.com>
2020-01-11 14:33:11 +0000

    updating fetch_version_spec to match appcenter spec style

--- spec/appcenter_fetch_version_number_spec.rb
+++ spec/appcenter_fetch_version_number_spec.rb
@@ -34,38 +34,34 @@ describe Fastlane::Actions::AppcenterFetchVersionNumberAction do
       allow(FastlaneCore::FastlaneFolder).to receive(:path).and_return(nil)
     end
 
-    context 'when no api token is given' do
-      it 'raises an error' do
+    context "check the correct errors are raised" do
+      it 'raises an error when no api token is given' do
         expect do
           Fastlane::FastFile.new.parse("lane :test do
-            latest_appcenter_build_number(
+            latest_appcenter_version_number(
               owner_name: 'owner-name',
               app_name: 'App-Name'
             )
           end").runner.execute(:test)
         end.to raise_error("No API token for AppCenter given, pass using `api_token: 'token'`")
       end
-    end
 
-    context 'when the app does not exist for a given owner/API Key' do
-      it 'raises an error' do
+      it 'raises an error when the app does not exist for a given owner/API key' do
         stub_get_apps_success(200)
         expect do
           Fastlane::FastFile.new.parse("lane :test do
-            latest_appcenter_build_number(
+            latest_appcenter_version_number(
               api_token: '1234',
               app_name: 'App-Name-Does-Not-Exist'
             )
           end").runner.execute(:test)
         end.to raise_error("No app 'App-Name-Does-Not-Exist' found for owner ")
       end
-    end
 
-    context 'when the app name contains spaces' do
-      it 'raises an error' do
+      it 'raises an error when the app name contains spaces' do
         expect do
           Fastlane::FastFile.new.parse("lane :test do
-            latest_appcenter_build_number(
+            latest_appcenter_version_number(
               api_token: '1234',
               owner_name: 'owner-name',
               app_name: 'App Name'
@@ -73,13 +69,11 @@ describe Fastlane::Actions::AppcenterFetchVersionNumberAction do
           end").runner.execute(:test)
         end.to raise_error("The `app_name` ('App Name') cannot contains spaces and must only contain alpha numeric characters and dashes")
       end
-    end
 
-    context 'when the app name contains special characters' do
-      it 'raises an error' do
+      it 'raises an error when the app name contains special characters' do
         expect do
           Fastlane::FastFile.new.parse("lane :test do
-            latest_appcenter_build_number(
+            latest_appcenter_version_number(
               api_token: '1234',
               owner_name: 'owner-name',
               app_name: 'App-Name!@£$'
@@ -87,13 +81,11 @@ describe Fastlane::Actions::AppcenterFetchVersionNumberAction do
           end").runner.execute(:test)
         end.to raise_error("The `app_name` ('App-Name!@£$') cannot contains spaces and must only contain alpha numeric characters and dashes")
       end
-    end
 
-    context 'when the owner name contains spaces' do
-      it 'raises an error' do
+      it 'raises an error when the owner name contains spaces' do
         expect do
           Fastlane::FastFile.new.parse("lane :test do
-            latest_appcenter_build_number(
+            latest_appcenter_version_number(
               api_token: '1234',
               owner_name: 'owner name',
               app_name: 'App-Name'
@@ -101,13 +93,11 @@ describe Fastlane::Actions::AppcenterFetchVersionNumberAction do
           end").runner.execute(:test)
         end.to raise_error("The `owner_name` ('owner name') cannot contains spaces and must only contain lowercased alpha numeric characters and dashes")
       end
-    end
 
-    context 'when the owner name contains special characters' do
-      it 'raises an error' do
+      it 'raises an error when the owner name contains special characters' do
         expect do
           Fastlane::FastFile.new.parse("lane :test do
-            latest_appcenter_build_number(
+            latest_appcenter_version_number(
               api_token: '1234',
               owner_name: '**/Owner name!!',
               app_name: 'App-Name'
@@ -115,14 +105,12 @@ describe Fastlane::Actions::AppcenterFetchVersionNumberAction do
           end").runner.execute(:test)
         end.to raise_error("The `owner_name` ('**/Owner name!!') cannot contains spaces and must only contain lowercased alpha numeric characters and dashes")
       end
-    end
 
-    context 'when the app name does not exist for an owner/account' do
-      it 'raises an error' do
+      it 'raises an error when the app name does not exist for an owner/account' do
         stub_get_releases_forbidden(403)
         expect do
           Fastlane::FastFile.new.parse("lane :test do
-            latest_appcenter_build_number(
+            latest_appcenter_version_number(
               api_token: '1234',
               owner_name: 'owner-name',
               app_name: 'App-Name'
@@ -130,14 +118,12 @@ describe Fastlane::Actions::AppcenterFetchVersionNumberAction do
           end").runner.execute(:test)
         end.to raise_error("API Key not valid for 'owner-name'. This will be because either the API Key or the `owner_name` are incorrect")
       end
-    end
 
-    context 'when the owner/account name or API key are incorrect' do
-      it 'raises an error' do
+      it 'raises an error when the owner/account name or API key are incorrect' do
         stub_get_releases_not_found(404)
         expect do
           Fastlane::FastFile.new.parse("lane :test do
-            latest_appcenter_build_number(
+            latest_appcenter_version_number(
               api_token: '1234',
               owner_name: 'owner-name',
               app_name: 'App-Name'
@@ -145,14 +131,12 @@ describe Fastlane::Actions::AppcenterFetchVersionNumberAction do
           end").runner.execute(:test)
         end.to raise_error("No app or owner found with `app_name`: 'App-Name' and `owner_name`: 'owner-name'")
       end
-    end
 
-    context 'when the no app versions exist' do
-      it 'raises an error' do
+      it 'raises an error when the no app versions exist' do
         stub_get_releases_empty_success(200)
         expect do
           Fastlane::FastFile.new.parse("lane :test do
-            latest_appcenter_build_number(
+            latest_appcenter_version_number(
               api_token: '1234',
               owner_name: 'owner-name',
               app_name: 'App-Name-no-versions'
@@ -162,98 +146,84 @@ describe Fastlane::Actions::AppcenterFetchVersionNumberAction do
       end
     end
 
-    let(:app) do
-      {
-      "display_name" => "My App Name",
-          "name" => 'App-Name',
-          "owner" => {
-            "display_name" => 'Owner Name',
-            "email" => 'test@example.com',
-            "name" => 'owner-name'
-          }
-    }
-    end
-
-    context 'when no app name or owner/account are set' do
-      let(:build_number) do
-        build_number = Fastlane::FastFile.new.parse("lane :test do
-          latest_appcenter_build_number(
-            api_token: '1234'
-          )
-        end").runner.execute(:test)
+    context "when no errors are expected" do
+      let(:app) do
+        {
+        "display_name" => "My App Name",
+            "name" => 'App-Name',
+            "owner" => {
+              "display_name" => 'Owner Name',
+              "email" => 'test@example.com',
+              "name" => 'owner-name'
+            }
+      }
       end
 
-      before do
+      before :each do
         allow(Fastlane::Actions::LatestAppcenterBuildNumberAction).to receive(:prompt_for_apps).and_return([app])
         stub_get_apps_success(200)
         stub_get_releases_success(200)
       end
 
-      it 'prompts for an owner/account and app name' do
-        allow(Fastlane::Actions::LatestAppcenterBuildNumberAction).to receive(:gets).and_return("1\n")
-        expect(build_number).to eq('1.0.4.105')
-      end
-    end
+      context "with a valid token" do
+        let(:build_number) do
+          build_number = Fastlane::FastFile.new.parse("lane :test do
+            latest_appcenter_version_number(
+              api_token: '1234'
+            )
+          end").runner.execute(:test)
+        end
 
-    context 'when no app name is set' do
-      let(:build_number) do
-        build_number = Fastlane::FastFile.new.parse("lane :test do
-          latest_appcenter_build_number(
-            api_token: '1234',
-            owner_name: 'owner-name'
-          )
-        end").runner.execute(:test)
-      end
-
-      before do
-        allow(Fastlane::Actions::LatestAppcenterBuildNumberAction).to receive(:prompt_for_apps).and_return([app])
-        stub_get_apps_success(200)
-        stub_get_releases_success(200)
+        it 'returns the correct version number' do
+          allow(Fastlane::Actions::LatestAppcenterBuildNumberAction).to receive(:gets).and_return("1\n")
+          expect(build_number).to eq('1.0.4.105')
+        end
       end
 
-      it 'prompts for an app name' do
-        expect(build_number).to eq('1.0.4.105')
-      end
-    end
+      context "with a valid token and owner name" do
+        let(:build_number) do
+          build_number = Fastlane::FastFile.new.parse("lane :test do
+            latest_appcenter_version_number(
+              api_token: '1234',
+              owner_name: 'owner-name'
+            )
+          end").runner.execute(:test)
+        end
 
-    context 'when no owner name is set' do
-      let(:build_number) do
-        build_number = Fastlane::FastFile.new.parse("lane :test do
-          latest_appcenter_build_number(
-            api_token: '1234',
-            app_name: 'App-Name'
-          )
-        end").runner.execute(:test)
-      end
-
-      before do
-        allow(Fastlane::Actions::LatestAppcenterBuildNumberAction).to receive(:prompt_for_apps).and_return([app])
-        stub_get_apps_success(200)
-        stub_get_releases_success(200)
+        it 'returns the correct version number' do
+          expect(build_number).to eq('1.0.4.105')
+        end
       end
 
-      it 'prompts for an app name' do
-        expect(build_number).to eq('1.0.4.105')
-      end
-    end
+      context "with a valid token and app name" do
+        let(:build_number) do
+          build_number = Fastlane::FastFile.new.parse("lane :test do
+            latest_appcenter_version_number(
+              api_token: '1234',
+              app_name: 'App-Name'
+            )
+          end").runner.execute(:test)
+        end
 
-    context 'when a valid owner, app name, and token build numbers are requested' do
-      let(:build_number) do
-        build_number = Fastlane::FastFile.new.parse("lane :test do
-          latest_appcenter_build_number(
-            api_token: '1234',
-            owner_name: 'owner-name',
-            app_name: 'App-Name'
-          )
-        end").runner.execute(:test)
-      end
-
-      before do
-        stub_get_releases_success(200)
+        it 'returns the correct version number' do
+          expect(build_number).to eq('1.0.4.105')
+        end
       end
 
-      it 'returns the version number correctly' do
-        expect(build_number).to eq('1.0.4.105')
+      context "with a valid token, owner name, and app name" do
+        let(:build_number) do
+          build_number = Fastlane::FastFile.new.parse("lane :test do
+            latest_appcenter_version_number(
+              api_token: '1234',
+              owner_name: 'owner-name',
+              app_name: 'App-Name'
+            )
+          end").runner.execute(:test)
+        end
+
+        it 'returns the correct version number' do
+          expect(build_number).to eq('1.0.4.105')
+        end
       end
     end
   end
