f22cbc3a0e7047c9b29a827602ce7fcc17a3eae3
Matt Morrissette <yinzara@gmail.com>
2020-05-04 14:54:44 -0700

    Add more complete tests for create_app

--- spec/appcenter_create_app_action_spec.rb
+++ spec/appcenter_create_app_action_spec.rb
@@ -34,12 +34,52 @@ describe Fastlane::Actions::AppcenterCreateAppAction do
           end").runner.execute(:test)
         end.to raise_error("An app named 'App-Name' owned by owner-name already existed")
       end
+      it 'raises an error when app creation fails' do
+        expect do
+          stub_create_app(400, 'App-Name', 'display name', 'iPhone', 'Objective-C-Swift', 'organization', 'owner-name', 'a secret value')
+          stub_check_app(404, 'App-Name', 'owner-name')
+          Fastlane::FastFile.new.parse("lane :test do
+            appcenter_create_app(
+              api_token: '1234',
+              owner_type: 'organization',
+              owner_name: 'owner-name',
+              app_name: 'App-Name',
+              app_display_name: 'display name',
+              app_os: 'iPhone',
+              app_platform: 'Objective-C-Swift',
+              error_on_create_existing: false
+            )
+          end").runner.execute(:test)
+        end.to raise_error("Unable to create 'App-Name' owned by owner-name")
+      end
     end
 
     context "when no errors are expected" do
       before :each do
         stub_create_app(200, 'App-Name', 'display name', 'iPhone', 'Objective-C-Swift', 'organization', 'owner-name', 'a secret value')
       end
+      context "with no existing app" do
+        let(:version) do
+          stub_check_app(404, 'App-Name', 'owner-name')
+          Fastlane::FastFile.new.parse("lane :test do
+            appcenter_create_app(
+              api_token: '1234',
+              owner_type: 'organization',
+              owner_name: 'owner-name',
+              app_name: 'App-Name',
+              app_display_name: 'display name',
+              app_os: 'iPhone',
+              app_platform: 'Objective-C-Swift'
+            )
+          end").runner.execute(:test)
+        end
+
+        it 'returns the correct app' do
+          puts version
+          expect(version["name"]).to eq('App-Name')
+        end
+      end
+
       context "with existing app and :error_on_create_existing false" do
         let(:version) do
           stub_check_app(200, 'App-Name', 'owner-name')
