2350255bb2564bc5e40b77bf5b6339cecdf650b6
Kyryl <k@fastspring.com>
2020-10-08 13:07:17 -0700

    Improve EdDSA and DSA signature handling to allow both

--- lib/fastlane/plugin/appcenter/helper/appcenter_helper.rb
+++ lib/fastlane/plugin/appcenter/helper/appcenter_helper.rb
@@ -550,18 +550,15 @@ module Fastlane
         return if dsa_signature.to_s == '' && ed_signature.to_s == ''
 
         url = "v0.1/apps/#{owner_name}/#{app_name}/releases/#{release_id}"
+        body = {
+          metadata: {}
+        }
+        
         if dsa_signature.to_s != ''
-            body = {
-              metadata: {
-                dsa_signature: dsa_signature
-              }
-            }
-        elsif ed_signature.to_s != ''
-            body = {
-              metadata: {
-                ed_signature: ed_signature
-              }
-            }
+          body["metadata"]["dsa_signature"] = dsa_signature
+        end
+        if ed_signature.to_s != ''
+          body["metadata"]["ed_signature"] = ed_signature
         end
 
         UI.message("DEBUG: PATCH #{url}") if ENV['DEBUG']
